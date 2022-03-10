b8023be3970891785a51ae7fb97ad0a54987c1ef
Carlos Viera <pisis_13_3@hotmail.com>
2022-03-09 21:29:01 -0500

    Agregando components

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserList from "./components/Usuarios/UserList";
+import UserForm from "./components/Usuarios/UserForm";
+import "bootswatch/dist/yeti/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/Navbar/Navbar";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/new-user" element={<UserForm />} />
+          <Route path="/update/:id" element={<UserForm />} />
+        </Routes>
+      </div>
+      <ToastContainer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
